40ded5eeb9a24c2ab762a86c1d1932b1deefb6d4
Bond <bond@ownwell.com>
2026-03-19 18:29:39 -0500

    Update tests

--- __fixtures__/core.ts
+++ __fixtures__/core.ts
@@ -1,13 +1,15 @@
-import type * as core from '@actions/core'
-import { vi } from 'vitest'
+import type * as core from '@actions/core';
+import { vi } from 'vite-plus/test';
 
-export const debug = vi.fn<typeof core.debug>()
-export const error = vi.fn<typeof core.error>()
-export const info = vi.fn<typeof core.info>()
-export const getInput = vi.fn<typeof core.getInput>()
-export const setOutput = vi.fn<typeof core.setOutput>()
-export const startGroup = vi.fn<typeof core.startGroup>()
-export const endGroup = vi.fn<typeof core.endGroup>()
-export const setFailed = vi.fn<typeof core.setFailed>()
-export const saveState = vi.fn<typeof core.saveState>()
-export const warning = vi.fn<typeof core.warning>()
+export const debug = vi.fn<typeof core.debug>();
+export const error = vi.fn<typeof core.error>();
+export const info = vi.fn<typeof core.info>();
+export const getInput = vi.fn<typeof core.getInput>();
+export const getState = vi.fn<typeof core.getState>();
+export const setOutput = vi.fn<typeof core.setOutput>();
+export const startGroup = vi.fn<typeof core.startGroup>();
+export const endGroup = vi.fn<typeof core.endGroup>();
+export const setFailed = vi.fn<typeof core.setFailed>();
+export const saveState = vi.fn<typeof core.saveState>();
+export const warning = vi.fn<typeof core.warning>();
+export const isDebug = vi.fn<typeof core.isDebug>().mockReturnValue(false);
